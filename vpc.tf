9b63d015122664149aa2c17e362ba82ae9c60500
Gracia Fernandez <graciafdez@gmail.com>
2017-11-04 16:16:30 +0000

    Add gateway to subnet.
    The Internet gateway is required to allow communication between
    instances inside a VPC and the Internet.
    
    More info:
    
    http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Internet_Gateway.html

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-    cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags {
-        Name = "main"
-    }
+  tags {
+    Name = "main"
+  }
 }
 
 resource "aws_subnet" "main" {
@@ -13,4 +13,22 @@ resource "aws_subnet" "main" {
   tags {
     Name = "Main"
   }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.main.id}"
 }
