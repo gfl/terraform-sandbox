aac6c1fcb5ca90a2929487afd0fe1e5929f35db3
Gracia Fernandez <graciafdez@gmail.com>
2017-11-25 19:31:50 +0000

    Route53 setup example

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "concourse_ip" {
   value = "${aws_instance.concourse.public_ip}"
 }
+
+output "name_servers" {
+  value = "${aws_route53_zone.primary.name_servers}"
+}
---
+++ route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "primary" {
+  name = "gfl.keytwine.com"
+}
+
+resource "aws_route53_record" "dev-ns" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "gfl.keytwine.com"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.primary.name_servers.0}",
+    "${aws_route53_zone.primary.name_servers.1}",
+    "${aws_route53_zone.primary.name_servers.2}",
+    "${aws_route53_zone.primary.name_servers.3}",
+  ]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "www.gfl.keytwine.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["gfl.github.io"]
+}
